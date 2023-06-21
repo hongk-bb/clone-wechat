3b5f18ecb049d5215d094368d530c3fbd98fc907
hongkbb <481640364@qq.com>
2023-06-22 02:24:12 +0800

    feat:角标封装

---
+++ components/free-ui/free-badge.vue
@@ -0,0 +1,24 @@
+<template>
+	<text class="free-badge bg-danger text-white rounded-circle font-sm" :class="badgeClass" :style="badgeStyle"><slot></slot></text>
+</template>
+
+<script>
+	export default {
+		props: {
+			badgeClass: {
+				type: String,
+				default: ""
+			},
+			badgeStyle:{
+				type:String,
+				default:""
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.free-badge{
+		padding-left: 14rpx;padding-right: 14rpx;padding-bottom: 6rpx;padding-top: 6rpx;
+	}
+</style>
