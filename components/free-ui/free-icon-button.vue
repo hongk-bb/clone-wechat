e3257479e6b18500b1612bfe7f360193a54dd610
hongkbb <481640364@qq.com>
2023-06-22 01:00:52 +0800

    feat:图标按钮组件封装

---
+++ components/free-ui/free-icon-button.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="flex align-center justify-center" 
+	hover-class="bg-hover-light" @click="$emit('click')"
+	style="height: 90rpx;width: 90rpx;">
+		<rich-text class="iconfont font-md"><slot></slot></rich-text>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
